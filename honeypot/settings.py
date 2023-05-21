129b794f60ce87837f49417b593edc4c266a6905
Zekeriya AY <zekeriya@zekeriyaay.com>
2023-05-22 00:21:14 +0300

    some variables added

--- honeypot/settings.py
+++ honeypot/settings.py
@@ -1,15 +1,24 @@
+# Server/Socket Settings
 BUFFER_SIZE = 1024
+CONNECTION_TIMEOUT = 6000  # Telnet default timeout value: 60sec
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 65400
 LOG_FILE = 'honeypot-server.log'
-SHELL_PROMPT = b'[DEMO-INC-SERVER-01 ~]# '
-BANNER_FILE = 'banner'
-BANNER = """
+
+
+# Honeypot/Linux Settings
+POT_HOSTNAME = 'DEMO-INC-SERVER-02'
+POT_USERNAME = 'anadolufs'
+POT_IP = '192.168.4.132'
+POT_BROADCAST = '192.168.4.135'
+POT_MASK = '255.255.255.248'
+SHELL_PROMPT = f'[{POT_HOSTNAME} ~]# '
+BANNER = f"""
     ========================================
                 DEMO INC. SERVER
     ========================================
-    # Hostname....: DEMO-INC-SERVER-01
-    # IP Address..: 192.168.1.5
+    # Hostname....: {POT_HOSTNAME}
+    # IP Address..: {POT_IP}
     # OS Version..: Debian 10.0
     # Uptime......: 1 day(s), 02h 03m 41s
     ========================================
